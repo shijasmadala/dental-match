657914bb380e6d7b3ca448aaf40d86828e88ef14
Mohammed Mashood PV <mohammed.mashood@sen-sei.in>
2023-09-21 15:59:25 +0530

    Reformatted the code

---
+++ app/src/main/java/com/example/dentalmatch/home/presentation/HomeFragmentViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.dentalmatch.home.presentation
+
+import androidx.lifecycle.ViewModel
+
+class HomeFragmentViewModel : ViewModel() {
+}
--- app/src/main/java/com/example/dentalmatch/home/prsentation/HomeFragmentViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.dentalmatch.home.prsentation
-
-class HomeFragmentViewModel {
-}
